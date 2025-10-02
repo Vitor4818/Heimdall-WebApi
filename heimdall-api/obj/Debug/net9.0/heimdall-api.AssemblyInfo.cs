68fae4cb3483e23e2aea9eb02e3ad3f6e3925845
Vitor4818 <vitorg.martins4818@gmail.com>
2025-10-01 22:51:40 -0300

    feat(auth): add user registration API integration

--- heimdall-api/obj/Debug/net9.0/heimdall-api.AssemblyInfo.cs
+++ heimdall-api/obj/Debug/net9.0/heimdall-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("heimdall-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56897009abd41d35e5485883d2fada429678275f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f33b2c50d2b4175b253cbf89b743901f2649b86")]
 [assembly: System.Reflection.AssemblyProductAttribute("heimdall-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("heimdall-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
