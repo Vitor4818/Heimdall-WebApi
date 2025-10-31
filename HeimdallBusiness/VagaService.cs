7b62e00785a10d0d522258c69e51e9eef134c603
Vitor4818 <vitorg.martins4818@gmail.com>
2025-10-31 00:41:54 -0300

    feat(vaga): adiciona service da entidade vaga

---
+++ HeimdallBusiness/VagaService.cs
@@ -0,0 +1,60 @@
+using HeimdallModel;
+using HeimdallData;
+using Microsoft.EntityFrameworkCore;
+
+namespace HeimdallBusiness
+{
+    public class VagaService
+    {
+        private readonly AppDbContext _context;
+
+        public VagaService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //Listar vagas
+        public IQueryable<VagaModel> ListarVagas()
+        {
+            return _context.Vaga.AsNoTracking().AsQueryable();
+        }
+
+        //Obter vagas por id
+        public VagaModel? ObterPorId(int id)
+        {
+            return _context.Vaga.Find(id);
+        }
+
+        //Cadastrar Vaga
+        public VagaModel CadastrarVaga(VagaModel vaga)
+        {
+            _context.Vaga.Add(vaga);
+            _context.SaveChanges();
+            return vaga;
+        }
+
+        //Atualizar vaga
+        public bool AtualizarVaga(VagaModel vaga)
+        {
+            var existente = _context.Vaga.Find(vaga.Id);
+            if (existente == null) return false;
+            existente.Codigo = vaga.Codigo;
+            existente.Ocupada = vaga.Ocupada;
+            existente.ZonaId = vaga.ZonaId;
+            _context.Update(existente);
+            _context.SaveChanges();
+            return true;
+        }
+        
+        //Remove vaga
+        public bool RemoverVaga(int id)
+        {
+            var vaga = _context.Vaga.Find(id);
+            if (vaga == null) return false;
+            _context.Vaga.Remove(vaga);
+            _context.SaveChanges();
+            return true;
+        }
+
+    }
+}
